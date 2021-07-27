810c6c3a5d976959edd0c9ef9fde8518a3544db5
jijiucheng <302926124@qq.com>
2021-07-27 18:57:45 +0800

    build：1、新增基础工程配置，创建 framework、demo、xcworkspace 文件

---
+++ Demo/Demo/JJCSwiftTools-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JJCSwiftTools-Demo
+//
+//  Created by mxgx on 2021/7/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
